3cc227ab5369ca1e7a1250ecb300eed43723a8bd
Adan Gomes <adangones55@gmail.com>
2026-03-11 12:34:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1000,8 +1000,9 @@ async function aplicarCupom() {
 
 
 // --- NOVAS CONFIGURAÇÕES DE HORÁRIO ---
+// --- NOVAS CONFIGURAÇÕES DE HORÁRIO ---
 const H_ABERTURA = 18;
-const M_ABERTURA = 00; // 18:00
+const M_ABERTURA = 30; // 18:30
 const H_FECHAMENTO = 22;
 const M_FECHAMENTO = 30; // 22:30
 
